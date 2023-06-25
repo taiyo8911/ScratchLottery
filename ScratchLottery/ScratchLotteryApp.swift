251057a93d4501a66e09abee66a815d1949c47ab
Taiyo Koshiba <solluna.1119@gmail.com>
2023-06-25 19:50:17 +0900

    Initial Commit

---
+++ ScratchLottery/ScratchLotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchLotteryApp.swift
+//  ScratchLottery
+//
+//  Created by Taiyo Koshiba on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ScratchLotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
